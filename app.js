7ac1d5e2a601a56ff541ae40c6cfb1510665b17b
taiwo2 <adetonat@ymail.com>
2021-10-04 16:47:40 +0100

    Fix linters

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ window.addEventListener('load', () => {
   const handleMenuBar = () => {
     document.getElementById('nav-mob').style.display = 'flex';
     const showNav = Array.from(document.querySelectorAll('hide-menu-bar'));
-    showNav.forEach(element => {element.style.display = 'none';})
-  }
+    showNav.forEach((element) => { element.style.display = 'none'; });
+  };
 
   const hideMenuBar = () => {
     document.getElementById('nav-mob').style.display = 'none';
     const hideNav = Array.from(document.querySelectorAll('hide-menu-bar'));
-    hideNav.forEach(element => { element.style.display = 'flex'})
-  }
+    hideNav.forEach((element) => { element.style.display = 'flex'; });
+  };
 
   document.getElementById('menu-bar').addEventListener('click', handleMenuBar);
-  document.getElementById('close-btn').addEventListener('click', hideMenuBar)
-  document.getElementById('btn-nav2').addEventListener('click', hideMenuBar)
-})
-
+  document.getElementById('close-btn').addEventListener('click', hideMenuBar);
+  document.getElementById('btn-nav2').addEventListener('click', hideMenuBar);
+});
