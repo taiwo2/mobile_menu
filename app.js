0a7b20d602db4dba88e3388110916bc465da1247
taiwo2 <adetonat@ymail.com>
2021-10-04 15:59:17 +0100

    Fix eventListener

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-windows.addEventListener('load', () => {
+window.addEventListener('load', () => {
   const handleMenuBar = () => {
-    document.get
+    document.getElementById('nav-mob').style.display = 'flex';
+    const showNav = Array.from(document.querySelectorAll('hide-menu-bar'));
+    showNav.forEach(element => {element.style.display = 'none';})
   }
+
+  const hideMenuBar = () => {
+    document.getElementById('nav-mob').style.display = 'none';
+    const hideNav = Array.from(document.querySelectorAll('hide-menu-bar'));
+    hideNav.forEach(element => { element.style.display = 'flex'})
+  }
+
+  document.getElementById('menu-bar').addEventListener('click', handleMenuBar);
+  document.getElementById('close-btn').addEventListener('click', hideMenuBar)
+  document.getElementById('btn-nav2').addEventListener('click', hideMenuBar)
 })
+
